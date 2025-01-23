623845fc3d1c4c14a07c92043b3b154b6718f4e8
Thiago Magano <thiagomagano1993@outlook.com>
2025-01-23 13:06:52 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+app.use('/', cors({
+  origin: 'https://noc.thiagomagano.com',
+  allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+  allowMethods: ['POST', 'GET', 'OPTIONS'],
+  exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+  maxAge: 600,
+  credentials: true,
+}))
 
 app.get('/', (c) => {
   return c.json({
     ok: true,
-    message: "N.O.C.U FUTSAL TEAM"
+    message: "N.O.C.U FUTSAL TEAM CORS"
   })
 })
 
